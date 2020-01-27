75dd9479f632e85e24f37065582972d1ca3c4724
Joshua Peters <peters.l.joshua@gmail.com>
2020-01-27 09:51:33 -0600

    test

---
+++ src/main/java/com/summonerswar/springblog/SummonersWarBlogApplication.java
@@ -0,0 +1,13 @@
+package com.summonerswar.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummonersWarBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummonersWarBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/summonerswar/springblog/SummonersWarBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summonerswar.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummonersWarBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
